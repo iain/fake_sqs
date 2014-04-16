8cdaad5bf6df79070614fc1d07b7e5be41f1ec96
Adam Mckaig <adam.mckaig@gmail.com>
2014-04-16 15:45:26 -0400

    Use /ping to test web interface

--- lib/fake_sqs/test_integration.rb
+++ lib/fake_sqs/test_integration.rb
@@ -55,7 +55,7 @@ module FakeSQS
     end
 
     def up?
-      @pid && connection.get("/").code.to_s == "200"
+      @pid && connection.get("/ping").code.to_s == "200"
     rescue Errno::ECONNREFUSED
       false
     end
