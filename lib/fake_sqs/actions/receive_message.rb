b612afdaf712f4176458d47c77b230afcf376732
gitolite tester <tester@example.com>
2014-12-07 22:08:22 -0500

    fix signature.. ?"

--- lib/fake_sqs/actions/receive_message.rb
+++ lib/fake_sqs/actions/receive_message.rb
@@ -8,7 +8,8 @@ module FakeSQS
         @responder = options.fetch(:responder)
       end
 
-      def call(name, params)
+      def call(params)
+        name = params['queue']
         queue = @queues.get(name)
         messages = queue.receive_message(params)
         @responder.call :ReceiveMessage do |xml|
