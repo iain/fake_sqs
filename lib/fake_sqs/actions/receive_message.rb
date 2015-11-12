7b3ccd793344d1ad31bae3983d335d9b7135d26f
nritholtz <nritholtz@gmail.com>
2015-11-12 11:08:54 -0500

    Add message_attributes to Message object as per SQS Message struct

--- lib/fake_sqs/actions/receive_message.rb
+++ lib/fake_sqs/actions/receive_message.rb
@@ -11,13 +11,30 @@ module FakeSQS
       def call(name, params)
         queue = @queues.get(name)
         messages = queue.receive_message(params)
+        filtered_message_attribute_names = []
+        params.select{|k,v | k =~ /MessageAttributeName\.\d+/}.each do |key, value|
+          filtered_message_attribute_names << value
+        end
         @responder.call :ReceiveMessage do |xml|
           messages.each do |receipt, message|
             xml.Message do
               xml.MessageId message.id
               xml.ReceiptHandle receipt
               xml.MD5OfBody message.md5
+              xml.MD5OfMessageAttributes message.message_attributes_md5
               xml.Body message.body
+              message.message_attributes.each do |attribute|
+                if filtered_message_attribute_names.include?("All") || filtered_message_attribute_names.include?(attribute)
+                  xml.MessageAttribute do
+                    xml.Name attribute["Name"]
+                    xml.Value do
+                      xml.StringValue attribute["Value.StringValue"] if attribute["Value.StringValue"]
+                      xml.BinaryValue attribute["Value.BinaryValue"] if attribute["Value.BinaryValue"]
+                      xml.DataType attribute["Value.DataType"]
+                    end
+                  end
+                end
+              end
             end
           end
         end
--- lib/fake_sqs/message.rb
+++ lib/fake_sqs/message.rb
@@ -3,13 +3,14 @@ require 'securerandom'
 module FakeSQS
   class Message
 
-    attr_reader :body, :id, :md5
+    attr_reader :body, :id, :md5, :message_attributes
     attr_accessor :visibility_timeout
 
     def initialize(options = {})
       @body = options.fetch("MessageBody")
       @id = options.fetch("Id") { SecureRandom.uuid }
       @md5 = options.fetch("MD5") { Digest::MD5.hexdigest(@body) }
+      @message_attributes = extract_attributes(options)
     end
 
     def expire!
@@ -30,6 +31,49 @@ module FakeSQS
         "Id" => id,
         "MD5" => md5,
       }
+    end
+
+    def message_attributes_md5
+      sorted_attributes = @message_attributes.sort { |a,b| a["Name"] <=> b["Name"] }
+
+      buffer = sorted_attributes.each_with_object([]) do |attribute, buffer|
+        add_string_to_buffer(attribute["Name"], buffer)
+        add_string_to_buffer(attribute["Value.DataType"], buffer)
+
+        if (attribute["Value.StringValue"])
+          buffer << 1
+          add_string_to_buffer(attribute["Value.StringValue"], buffer)
+        elsif (attribute["Value.BinaryValue"])
+          buffer << 2
+          add_binary_to_buffer(attribute["Value.BinaryValue"], buffer)
+        end
+      end
+      Digest::MD5.hexdigest(buffer.pack("C*"))
+    end
+
+    def add_string_to_buffer(string, buffer)
+      string_bytes = string.force_encoding('UTF-8').bytes.to_a
+      buffer.concat [string_bytes.size].pack("N").bytes.to_a
+      buffer.concat string_bytes
+    end
+
+    def add_binary_to_buffer(binary, buffer)
+      binary_bytes = binary.unpack("m*")[0].bytes.to_a
+      buffer.concat [binary_bytes.size].pack("N").bytes.to_a
+      buffer.concat binary_bytes
+    end
+
+    private
+    def extract_attributes(options)
+      attributes = []
+      options.each {|key, value|
+        if /MessageAttribute\.(?<attr_index>\d+)\.(?<attr_name>.*)/ =~ key
+          index = attr_index.to_i - 1
+          attributes[index] = Hash.new unless attributes[index]
+          attributes[index][attr_name] = value
+        end
+      }
+      attributes
     end
 
   end
