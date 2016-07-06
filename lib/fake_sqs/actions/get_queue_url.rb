56769738f6bd333716aff476f59419c327d96cb8
Christopher Currie <codemonkey@usermind.com>
2016-07-06 13:46:11 -0700

    Use host/port from request on queue URL

--- lib/fake_sqs/actions/get_queue_url.rb
+++ lib/fake_sqs/actions/get_queue_url.rb
@@ -6,13 +6,14 @@ module FakeSQS
         @server    = options.fetch(:server)
         @queues    = options.fetch(:queues)
         @responder = options.fetch(:responder)
+        @request   = options.fetch(:request)
       end
 
       def call(params)
         name = params.fetch("QueueName")
         queue = @queues.get(name, params)
         @responder.call :GetQueueUrl do |xml|
-          xml.QueueUrl @server.url_for(queue.name)
+          xml.QueueUrl @server.url_for(queue.name, {:host => @request.host, :port => @request.port})
         end
       end
 
--- lib/fake_sqs/actions/list_queues.rb
+++ lib/fake_sqs/actions/list_queues.rb
@@ -6,13 +6,14 @@ module FakeSQS
         @server    = options.fetch(:server)
         @queues    = options.fetch(:queues)
         @responder = options.fetch(:responder)
+        @request   = options.fetch(:request)
       end
 
       def call(params)
         found = @queues.list(params)
         @responder.call :ListQueues do |xml|
           found.each do |queue|
-            xml.QueueUrl @server.url_for(queue.name)
+            xml.QueueUrl @server.url_for(queue.name, {:host => @request.host, :port => @request.port})
           end
         end
       end
